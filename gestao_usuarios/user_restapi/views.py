552154a22d8848577cd61afcee6b148a682929d5
Diego Varela <diego.varela@corp.globo.com>
2020-06-29 13:46:07 -0300

    [user_restapi][views][User] Removendo o controle de permissão

--- gestao_usuarios/user_restapi/views.py
+++ gestao_usuarios/user_restapi/views.py
@@ -2,11 +2,11 @@ from rest_framework import viewsets
 from rest_framework import permissions
 from user_restapi.models import User
 from user_restapi.serializers import UserSerializer
-from user_restapi.permissions import IsOwnerOrReadOnly
+#from user_restapi.permissions import IsOwnerOrReadOnly
 
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly,
-                          IsOwnerOrReadOnly]
+#    permission_classes = [permissions.IsAuthenticatedOrReadOnly,
+#                          IsOwnerOrReadOnly]
