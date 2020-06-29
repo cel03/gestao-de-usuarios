41c3125eb036d945b1c6a7fd7ab2cb23e8cb03a7
Diego Varela <diego.varela@corp.globo.com>
2020-06-29 11:02:40 -0300

    [seetings] instalando e configurando o cors + [requirements] sql

--- gestao_usuarios/gestao_usuarios/settings.py
+++ gestao_usuarios/gestao_usuarios/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'user_restapi'
+    'user_restapi',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -49,7 +50,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'gestao_usuarios.urls'
 
